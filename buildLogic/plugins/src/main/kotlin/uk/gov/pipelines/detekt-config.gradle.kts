a868640c3763e20738fb01ad9a05b5cada45d0af
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2024-10-28 09:47:01 +0000

    fix: Don't require detekt config override

--- buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/detekt-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/detekt-config.gradle.kts
@@ -7,13 +7,17 @@ project.plugins.apply("io.gitlab.arturbosch.detekt")
 configure<DetektExtension>(setupDetekt())
 
 fun setupDetekt(): DetektExtension.() -> Unit = {
-    val configDir = "${project.rootProject.projectDir}/config"
-    logger.info("Detekt configuration: Using config directory: $configDir")
+    val configOverrideFile = file(
+        "${project.rootProject.projectDir}/config/detekt/detektConfig.yml"
+    )
     val detektToolVersion = "1.23.1"
 
     allRules = true
     buildUponDefaultConfig = true
-    config.from(file("$configDir/detekt/detektConfig.yml"))
+    if (configOverrideFile.exists()) {
+        logger.info("Detekt configuration: Using config file: $configOverrideFile")
+        config.from(configOverrideFile)
+    }
     debug = false
     disableDefaultRuleSets = false
     ignoreFailures = false
