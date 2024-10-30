5b5e59f41d5e4809b941abe8ada3a026dcd2d40d
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2024-10-30 16:18:43 +0000

    feat: Add default detekt rule for composables (#8)
    The default detekt configuration does not allow function names to start with a capital letter meaning that Composable functions are flagged as errors.
    
    - Add default detekt configuration
    - Add default detekt rule to allow functions to start with a capital letter (as per [detekt docs])
    
    Also
    - Update detekt
    - Add a composable to the test project
    - Add a compose test to the test project
    
    DCMAW-10478
    
    [detekt docs]: https://detekt.dev/docs/introduction/compose/#functionnaming-for-compose

--- buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/detekt-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/detekt-config.gradle.kts
@@ -1,19 +1,24 @@
 package uk.gov.pipelines
 
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import uk.gov.pipelines.config.buildLogicDir
 
 project.plugins.apply("io.gitlab.arturbosch.detekt")
 
 configure<DetektExtension>(setupDetekt())
 
 fun setupDetekt(): DetektExtension.() -> Unit = {
+    val configDefaultFile = file(
+        "${project.buildLogicDir}/config/detekt/detektConfig.yml"
+    )
     val configOverrideFile = file(
         "${project.rootProject.projectDir}/config/detekt/detektConfig.yml"
     )
-    val detektToolVersion = "1.23.1"
+    val detektToolVersion = "1.23.7"
 
     allRules = true
     buildUponDefaultConfig = true
+    config.from(configDefaultFile)
     if (configOverrideFile.exists()) {
         logger.info("Detekt configuration: Using config file: $configOverrideFile")
         config.from(configOverrideFile)
--- test-project/test-library/build.gradle.kts
+++ test-project/test-library/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("uk.gov.pipelines.android-lib-config")
+    alias(libs.plugins.kotlin.compose.compiler)
 }
 
 android {
@@ -7,10 +8,16 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.test.core.ktx)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
     androidTestUtil(libs.androidx.test.orchestrator)
 }
