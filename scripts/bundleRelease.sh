2185bdd49d4b9245f01fd9db9ebd9c037140a457
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2024-12-11 14:15:54 +0000

    build: update bundleRelease script funcitonality and comments

--- scripts/bundleRelease.sh
+++ scripts/bundleRelease.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-# Example: `MODULES_TO_BUNDLE=app,modules/testWrapper INPUT_FLAVORS=staging,integration scripts/bundleRelease.sh`
+# Example: `MODULES_TO_BUNDLE=app,my/app,your:app INPUT_FLAVORS=build,dev scripts/bundleRelease.sh`
 
-# Converting comma separated string input into a list of strings for module paths.
-IFS="," read -r -a MODULE_PATHS <<< "${MODULES_TO_BUNDLE}"
-echo "${MODULE_PATHS[@]}"
+# Converting comma separated string input into a list of strings, replacing ":" with "/" for
+# module paths.
+IFS="," read -r -a MODULE_PATHS <<< "${MODULES_TO_BUNDLE/://}"
+echo Module paths: "${MODULE_PATHS[@]}"
 
 # Converting comma separated string input into a list of strings for input flavors.
 IFS="," read -r -a FLAVORS <<< "${INPUT_FLAVORS}"
-echo "${FLAVORS[@]}"
+echo Flavors: "${FLAVORS[@]}"
 
 # Converting comma separated string input into a list of strings, replacing "/" with ":" for tasks.
 IFS="," read -r -a MODULE_TASKS <<< "${MODULES_TO_BUNDLE////:}"
-echo "${MODULE_TASKS[@]}"
+echo Modules to run gradle task: "${MODULE_TASKS[@]}"
 
 
 # Requires one argument for the module to bundle release to run bundle release task.
