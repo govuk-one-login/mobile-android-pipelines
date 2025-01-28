a32847d477ada893c531908b36c020381a2293c3
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-28 16:28:31 +0000

    chore: address PR comments

--- scripts/checkPackagesPublishedLocally.sh
+++ scripts/checkPackagesPublishedLocally.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# Example: `EXPECTED_PACKAGES=uk.gov.my.app,uk.gov.publishing.your-app scripts/checkPackagesPublishedLocally.sh`
+# Example: `EXPECTED_PACKAGES=uk.gov.my:app,uk.gov.publishing:your-app scripts/checkPackagesPublishedLocally.sh`
 
 # Converting comma separated string input `EXPECTED_PACKAGES` into a list of strings,
-# replacing "." with "/" to make it a list of directory path.
-IFS="," read -r -a PACKAGES <<< "${EXPECTED_PACKAGES//.//}"
+# replacing "." and then ":" with "/" to make it a list of directory path.
+PACKAGES="${EXPECTED_PACKAGES//.//}"
+IFS="," read -r -a PACKAGES <<< "${PACKAGES//://}"
 echo Packages: "${PACKAGES[@]}"
 
 # Navigate to directory where publishToMavenLocal publishes to in filesystem. If this local
