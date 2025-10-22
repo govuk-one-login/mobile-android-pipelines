d594a0d57cd460eb3caee8ccdf2bdc7b037f2e1e
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2025-10-22 16:58:40 +0100

    test: Parameterize EmulatorConfigPluginTest
    - Find the `BaseExtension` instead of a specific android extension, as
      that's the datatype that's being configured within the plugin.

--- buildLogic/plugins/src/test/kotlin/uk/gov/pipelines/EmulatorConfigPluginTest.kt
+++ buildLogic/plugins/src/test/kotlin/uk/gov/pipelines/EmulatorConfigPluginTest.kt
@@ -1,12 +1,13 @@
 package uk.gov.pipelines
 
-import com.android.build.api.dsl.LibraryExtension
+import com.android.build.gradle.BaseExtension
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.testfixtures.ProjectBuilder
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import uk.gov.pipelines.EmulatorConfigProjectExtensions.exampleEmulatorConfig
 import uk.gov.pipelines.EmulatorConfigProjectExtensions.setupEmulatorConfigExtras
 import uk.gov.pipelines.ManagedDevicesMatchers.hasSize
@@ -20,21 +21,25 @@ class EmulatorConfigPluginTest {
         project.setupEmulatorConfigExtras(
             emulatorConfig = exampleEmulatorConfig,
         )
-
-        listOf(
-            "com.android.library",
-            "uk.gov.pipelines.emulator-config",
-        ).forEach(project.pluginManager::apply)
     }
 
-    @Test
-    fun `Plugin creates a device based on EmulatorConfig`() {
-        project.extensions.findByType<LibraryExtension>()?.let { extension ->
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "com.android.application",
+            "com.android.library",
+        ],
+    )
+    fun `Plugin creates a device based on EmulatorConfig`(androidPlugin: String) {
+        listOf(
+            androidPlugin,
+            "uk.gov.pipelines.emulator-config",
+        ).forEach(project.pluginManager::apply)
+
+        project.extensions.findByType<BaseExtension>()?.let { extension ->
             assertThat(
                 extension.testOptions,
-                hasManagedDevices(
-                    hasSize(1),
-                ),
+                hasManagedDevices(hasSize(1)),
             )
         } ?: fail { "Cannot find android library extension to verify managed devices!" }
     }
