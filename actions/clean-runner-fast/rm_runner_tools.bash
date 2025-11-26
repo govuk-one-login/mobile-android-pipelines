a212eb93addf367e1221a56a3af091ab6b254c9d
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-11-26 16:20:42 +0000

    chore: remove php

--- actions/clean-runner-fast/rm_runner_tools.bash
+++ actions/clean-runner-fast/rm_runner_tools.bash
@@ -7,14 +7,29 @@ formatByteCount() { echo $(numfmt --to=iec-i --suffix=B --padding=7 $1'000'); }
 
 BEFORE=$(getAvailableSpace)
 
-unused_tools=(
+which php
+
+echo "user local:"
+ls /usr/local
+
+echo "user share:"
+ls /usr/share
+
+echo "opt"
+ls /opt/microsoft
+
+UNUSED_TOOLS=(
   "/usr/share/dotnet"      # .NET
+  "/usr/share/mercurial"   # Mercurial
+  "/usr/share/php*"        # PHP
+  "/usr/share/swift"       # Swift
   "/opt/ghc"               # Haskell
   "/usr/local/.ghcup"      # Haskell
+  "/opt/microsoft"         # Microsoft
   "/usr/local/share/boost" # Boost C++
 )
 
-rm -rf "${unused_tools[@]}"
+rm -rf "${UNUSED_TOOLS[@]}"
 
 sudo swapoff -a
 sudo rm -f /mnt/swapfile
