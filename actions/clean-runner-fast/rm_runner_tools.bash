1b07f6cc24a7135c887985c068d3576603fb6aec
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-11-26 15:28:03 +0000

    test: attempt to save more space for runner

--- actions/clean-runner-fast/rm_runner_tools.bash
+++ actions/clean-runner-fast/rm_runner_tools.bash
@@ -2,10 +2,25 @@
 
 # https://github.com/actions/runner-images/issues/2840#issuecomment-790492173
 
+getAvailableSpace() { echo $(df -a $1 | awk 'NR > 1 {avail+=$4} END {print avail}'); }
+formatByteCount() { echo $(numfmt --to=iec-i --suffix=B --padding=7 $1'000'); }
+
+BEFORE=$(getAvailableSpace)
+
 unused_tools=(
   "/usr/share/dotnet"      # .NET
   "/opt/ghc"               # Haskell
+  "/usr/local/.ghcup"      # Haskell
   "/usr/local/share/boost" # Boost C++
 )
 
 rm -rf "${unused_tools[@]}"
+
+sudo swapoff -a
+sudo rm -f /mnt/swapfile
+free -h
+
+AFTER=$(getAvailableSpace)
+SAVED=$((AFTER-BEFORE))
+
+echo "Saved: $(formatByteCount $SAVED)"
