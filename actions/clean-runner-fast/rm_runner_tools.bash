f78fe8c8aada280478f652cd893918ca54f71f87
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-12-04 11:35:20 +0000

    chore: address code review comment

--- actions/clean-runner-fast/rm_runner_tools.bash
+++ actions/clean-runner-fast/rm_runner_tools.bash
@@ -2,6 +2,8 @@
 
 # https://github.com/actions/runner-images/issues/2840#issuecomment-790492173
 
+NUM_TO_REMOVE=$1
+
 getAvailableSpace() { echo $(df -a $1 | awk 'NR > 1 {avail+=$4} END {print avail}'); }
 formatByteCount() { echo $(numfmt --to=iec-i --suffix=B --padding=7 $1'000'); }
 
@@ -18,7 +20,7 @@ UNUSED_TOOLS=(
   "/usr/local/share/boost" # Boost C++
 )
 
-rm -rf "${UNUSED_TOOLS[@]: 0:$1}"
+rm -rf "${UNUSED_TOOLS[@]: 0:$NUM_TO_REMOVE}"
 
 AFTER=$(getAvailableSpace)
 SAVED=$((AFTER-BEFORE))
