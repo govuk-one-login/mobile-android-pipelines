3eb7b59439b01616510c25b825a907f0d5350653
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-12-04 11:55:35 +0000

    feat: save more space with runner script (#244)
    * test: attempt to save more space for runner
    
    * chore: remove php
    
    * chore: remove disabling swap
    
    * feat: enable consumer to specify the level of cleaning required
    
    * chore: address code review comment

--- actions/clean-runner-fast/rm_runner_tools.bash
+++ actions/clean-runner-fast/rm_runner_tools.bash
@@ -2,10 +2,27 @@
 
 # https://github.com/actions/runner-images/issues/2840#issuecomment-790492173
 
-unused_tools=(
+NUM_TO_REMOVE=$1
+
+getAvailableSpace() { echo $(df -a $1 | awk 'NR > 1 {avail+=$4} END {print avail}'); }
+formatByteCount() { echo $(numfmt --to=iec-i --suffix=B --padding=7 $1'000'); }
+
+BEFORE=$(getAvailableSpace)
+
+UNUSED_TOOLS=(
   "/usr/share/dotnet"      # .NET
+  "/usr/share/mercurial"   # Mercurial
+  "/usr/share/php*"        # PHP
+  "/usr/share/swift"       # Swift
   "/opt/ghc"               # Haskell
+  "/usr/local/.ghcup"      # Haskell
+  "/opt/microsoft"         # Microsoft
   "/usr/local/share/boost" # Boost C++
 )
 
-rm -rf "${unused_tools[@]}"
+rm -rf "${UNUSED_TOOLS[@]: 0:$NUM_TO_REMOVE}"
+
+AFTER=$(getAvailableSpace)
+SAVED=$((AFTER-BEFORE))
+
+echo "Saved: $(formatByteCount $SAVED)"
