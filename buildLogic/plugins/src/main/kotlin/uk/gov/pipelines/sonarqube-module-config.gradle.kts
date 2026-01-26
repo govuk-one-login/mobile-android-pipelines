de2ed0cc890527ba3e264f970be9bef1aa7df5b6
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-01-26 09:45:38 +0000

    feat: revert changes to module config gradle file

--- buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/sonarqube-module-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/sonarqube-module-config.gradle.kts
@@ -1,67 +1,94 @@
 package uk.gov.pipelines
 
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.findByType
-import org.gradle.testfixtures.ProjectBuilder
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Test
-import org.sonarqube.gradle.ActionBroadcast
 import org.sonarqube.gradle.SonarExtension
-import org.sonarqube.gradle.SonarProperties
+import java.io.File
 
-class SonarqubeModuleConfigPluginTest {
-    private val project = ProjectBuilder.builder().build()
-
-    @Test
-    fun `it applies sonar plugin`() {
-        project.pluginManager.apply(SonarqubeModuleConfigPlugin::class.java)
-
-        assertNotNull(project.extensions.findByType<SonarExtension>())
-    }
-
-    @Test
-    fun `it configures sonar extension`() {
-        val properties = mutableMapOf<String, Any>()
-
-        project.applySonarModuleConfigPlugin(outputProperties = properties)
-
-        assertEquals(
-            EXCLUSIONS,
-            properties["sonar.exclusions"],
-        )
-    }
-
-    private fun Project.applySonarModuleConfigPlugin(outputProperties: MutableMap<String, Any>) {
-        val sonarProperties = SonarProperties(outputProperties)
-        val propertiesActions = ActionBroadcast<SonarProperties>()
-        extensions.create(
-            "testSonar",
-            SonarExtension::class.java,
-            propertiesActions,
-        )
-        pluginManager.apply(SonarqubeModuleConfigPlugin::class.java)
-
-        propertiesActions.execute(sonarProperties)
-    }
+plugins {
+    id("org.sonarqube")
 }
 
-private const val EXCLUSIONS =
-    "**/R.class,**/R$*.class,**/BuildConfig.*,**/Manifest*.*,**/*Test*.*,android/**/*.*," +
-            "**/*FileManager*,**/*AndroidCamera*,**/*AndroidBiometrics*,**/*ContactsProvider*," +
-            "**/*IntentProvider*,android/databinding/**/*.class," +
-            "**/android/databinding/*Binding.class,**/android/databinding/*," +
-            "**/androidx/databinding/*,**/databinding/*,**/BR.*,**/*_MembersInjector.class," +
-            "**/Dagger*Component.class,**/Dagger*Component\$Builder.class," +
-            "**/Dagger*Subcomponent*.class,**/*Subcomponent\$Builder.class," +
-            "**/*Module_*Factory.class,**/dagger/hilt/internal/**/*.*," +
-            "**/di/module/*,**/*_Factory*.*,**/*Module*.*,**/*Dagger*.*,**/*Hilt*.*," +
-            "**/*DependenciesProvider*,**/*_GeneratedInjector.*,**/*Args.class,**/*Directions.*," +
-            "**/*MapperImpl*.*,**/*\$ViewInjector*.*,**/*\$ViewBinder*.*,**/BuildConfig.*," +
-            "**/*Component*.*,**/*BR*.*,**/Manifest*.*,**/*\$Lambda$*.*,**/*Companion*.*," +
-            "**/*MembersInjector*.*,**/*_MembersInjector.class,**/*_Factory*.*," +
-            "**/*_Provide*Factory*.*,**/*Extensions*.*,**/*Extension*.*,**/*\$Result.*," +
-            "**/*\$Result$*.*,*.json,**/.gradle/**,**/*.gradle*,**/src/test/java/$," +
-            "**/src/test*/java/$,**/src/androidTest*/java/$,**/src/androidTest/java/$," +
-            "**/src/*/java/**/developer/**,**/src/*/kotlin/**/developer/**," +
-            "**/src/*/java/**/developer/**,**/src/*/kotlin/**/developer/**"
+fun generateCommaSeparatedFiles(iterator: Iterable<String>) =
+    fileTree(project.projectDir) {
+        this.setIncludes(iterator)
+    }.files.joinToString(
+        separator = ",",
+        transform = File::getAbsolutePath,
+    )
+
+val androidLintReportFiles by project.extra(
+    generateCommaSeparatedFiles(listOf("**/reports/lint-results-*.xml")),
+)
+val detektReportFiles by project.extra(
+    generateCommaSeparatedFiles(
+        listOf(
+            "**/reports/detekt/*.xml",
+        ),
+    ),
+)
+val jacocoXmlReportFiles by project.extra(
+    generateCommaSeparatedFiles(
+        listOf(
+            // unit test reports, split to stop vale reading line
+            "**/reports/jacoco" +
+                "/**/*.xml",
+            // android instrumentation test reports
+            "**/reports/coverage/**/*.xml",
+        ),
+    ),
+)
+val junitReportFiles by project.extra(
+    generateCommaSeparatedFiles(
+        listOf(
+            // instrumentation
+            "**/outputs/androidTest-results/managedDevice/*",
+            // unit tests
+            "**/test-results",
+        ),
+    ),
+)
+val ktLintReportFiles by project.extra(
+    generateCommaSeparatedFiles(listOf("**/reports/ktlint/**/*.xml")),
+)
+val sonarExclusions by project.extra(
+    listOf(
+        Filters.androidInstrumentationTests,
+        Filters.sonar,
+        Filters.testSourceSets,
+        Filters.developer,
+        Filters.uiTestWrapper,
+    ).flatten().joinToString(separator = ","),
+)
+
+val moduleSourceFolder = SourceSetFolder(project)
+var sourceFolders by project.extra("")
+var testFolders by project.extra("")
+
+var projectSonarProperties by project.extra(
+    mapOf<String, Any>(),
+)
+
+configure<SonarExtension> {
+    if (moduleSourceFolder.srcExists()) {
+        sourceFolders = moduleSourceFolder.sourceFolders
+        testFolders = moduleSourceFolder.testFolders
+    }
+
+    projectSonarProperties =
+        mapOf<String, Any>(
+            "sonar.sources" to sourceFolders,
+            "sonar.tests" to testFolders,
+            "sonar.exclusions" to sonarExclusions,
+            "sonar.androidLint.reportPaths" to androidLintReportFiles,
+            "sonar.coverage.jacoco.xmlReportPaths" to jacocoXmlReportFiles,
+            "sonar.kotlin.detekt.reportPaths" to detektReportFiles,
+            "sonar.kotlin.ktlint.reportPaths" to ktLintReportFiles,
+            "sonar.junit.reportPaths" to junitReportFiles,
+        )
+
+    properties {
+        projectSonarProperties.forEach { (key: String, value: Any) ->
+            property(key, value)
+            project.logger.debug("SONAR $key $value")
+        }
+    }
+}
