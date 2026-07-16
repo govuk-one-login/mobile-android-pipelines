31fffe64abd0f03b84e02dfbdd34a4c7825a9747
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-16 09:18:38 +0100

    feat!: DCMAW-20946 Improve performance of actions/setup-runner (#363)
    Adds new parameters for compatibility, to allow accepting Android licences:
    
    - Adds accept-android-licences parameter to actions/setup-runner
    - Adds ACCEPT_ANDROID_LICENCES parameter to the main action

--- scripts/runInstrumentationTests.sh
+++ scripts/runInstrumentationTests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew \
-  googleAtdPixelXLApi33DebugAndroidTest \
+  allDevicesDebugAndroidTest \
   --continue \
   --stacktrace \
   -Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect
--- test-project/build.gradle.kts
+++ test-project/build.gradle.kts
@@ -40,10 +40,8 @@ val emulatorConfig by rootProject.extra(
     EmulatorConfig(
         systemImageSources = setOf(SystemImageSource.AOSP_ATD),
         androidApiLevels = setOf(
-            // Cannot use Android Test Device (ATD) below API 30
-            30,
-            // The emulator would not start in the Github Actions runner when using API 34
             33,
+            35,
         ),
         deviceFilters = setOf("Pixel XL"),
     )
