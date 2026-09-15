548f9f9b7c8931a56c7bbb6c6386d26c4e58001b
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-15 20:28:20 +0100

    fix: Fix possibility of stale test devices cache (#391)
    The `uk.gov.pipelines.emulator-config` plugin creates managed device configurations and respective test tasks.
    
    Before this change the plugin tried to avoid creating devices that aren't available on the system, however:
    
    1. This doesn't work reliably - when local devices change, the cached device list isn't updated until the developer manually deletes the cache file.
    2. It isn't clear to the developer why a test task is unavailable - there is no error explaining that the device wasn't found on the system
    
    This change removes this caching and filtering logic to make these types of problems clearer and easier to resolve for developers.
    
    ### Before
    
    When a device is unavailable, the test task is not found. And even when the missing device is subsequently installed, the task is still not created.
    
    ```sh
    $ ./gradlew tasks | grep `Atd`
    # no output
    
    $ ./gradlew aospAtdPixelXLApi35DebugAndroidTest
    Task 'aospAtdPixelXLApi35DebugAndroidTest' not found
    BUILD FAILED
    ```
    
    To resolve the issue, the developer must manually delete `managedDeviceHardwareProfiles.txt`.
    
    ### After
    
    When a device is unavailable, the task is created, but it fails with a clear error when run:
    ```sh
    $ ./gradlew tasks | grep `Atd`
    aospAtdNotARealDeviceApi35DebugAndroidTest - Installs and runs the test for debug  on the managed device aospAtdNotARealDeviceApi35
    
    $ ./gradlew aospAtdNotARealDeviceApi35DebugAndroidTest
    Execution failed for task ':componentsv2:aospAtdNotARealDeviceApi35Setup' (registered by plugin 'com.android.internal.library').
    ...
       > java.util.concurrent.ExecutionException: java.lang.IllegalStateException: Failed to find hardware profile for name: NotARealDevice
         Try one of the following device profiles: Pixel 6, Pixel C, Pixel 2, Pixel 3, Pixel 4
    ```
    
    When the device is installed, the test task works without needing to clear any caches.

--- buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/emulator-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/pipelines/emulator-config.gradle.kts
@@ -4,8 +4,6 @@ import com.android.build.api.dsl.CommonExtension
 import uk.gov.pipelines.emulator.EmulatorConfig
 import uk.gov.pipelines.extensions.BaseExtensions.generateDeviceConfigurations
 import uk.gov.pipelines.extensions.BaseExtensions.generateGetHardwareProfilesTask
-import java.io.BufferedReader
-import java.io.FileReader
 
 val emulatorConfig: EmulatorConfig by rootProject.extra
 
@@ -18,40 +16,9 @@ val emulatorConfig: EmulatorConfig by rootProject.extra
  * made.
  */
 configure<CommonExtension> {
-    val hardwareProfilesList =
-        rootProject.layout.buildDirectory.file(
-            "outputs/managedDeviceHardwareProfiles.txt",
-        )
-
-    val hardwareProfilesTask = generateGetHardwareProfilesTask(project, hardwareProfilesList)
-
-    if (!hardwareProfilesList.map { it.asFile.exists() }.get()) {
-        /**
-         * Call the hardware profiles task within the gradle configuration stage for the sake of
-         * building out the various hardware profiles.
-         */
-        providers
-            .exec {
-                commandLine = hardwareProfilesTask.get().commandLine
-                args = hardwareProfilesTask.get().args
-            }.result
-            .get()
-            .exitValue
-    }
-
-    val hardwareProfileStrings: List<String> =
-        BufferedReader(
-            hardwareProfilesList.map { FileReader(it.asFile) }.get(),
-        ).readLines()
-            .filter { profile ->
-                emulatorConfig.deviceFilters.any { filter ->
-                    profile.contains(filter, ignoreCase = true)
-                }
-            }
-
     generateDeviceConfigurations(
         androidApiLevels = emulatorConfig.androidApiLevels,
-        hardwareProfileStrings = hardwareProfileStrings,
+        hardwareProfileStrings = emulatorConfig.deviceFilters,
         systemImageSources = emulatorConfig.systemImageSources,
     )
 }
